dc5af8969e01ddac2b15fde15660b51e63631351
kurkachatka <marta.witkowskaa@gmail.com>
2025-05-07 17:15:57 +0200

    Setting up models

--- Intro-to-RMark.R
+++ Intro-to-RMark.R
@@ -20,5 +20,41 @@ aa.process <- process.data(aa, model="CJS", groups="colony")
 aa.ddl <- make.design.data(aa.process)
 
 
+# create the fucntion with models of interest
+# components of the model Phi and p will be combined with each other
+
+aa.models <-  function()
+{
+  # models for Phi (apparent survival)
+  Phi.const <- list(formula = ~1) # simple, intercept only model assuming constant apparent survival
+  Phi.time <- list(formula = ~time) # model allowing for apparent survival to vary in time
+  # there is a difference between time and Time, the later checks for trend in the survival variation
+  
+  Phi.colony <- list(formula = ~colony) # using grouping factor as variable to influence apparent survival
+  Phi.colony.time <- list(formula = ~time*colony) # checking for effect of intereaction + additive effect
+  # to check just for the effect interaction ':' works
+  
+  
+  # models for p (encounter probablity)
+  
+  p.dot <- list(formula = ~1) # contant
+  p.time <- list(formula = ~time) # time dependent
+  
+  
+  cml <- create.model.list("CJS")
+  
+  results <- mark.wrapper(cml,data = aa.process, ddl = aa.ddl, adjust = FALSE)
+  
+  return(results)
+}
+
+
+
+
+
+
+aa.results <- aa.models() # run the model
+
+aa.results # see the ranking of models
 
 
